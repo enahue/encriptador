b3588d2eb17657acf7a768bb9fd5a8246dcdec09
erwinnahue <erwinnahue@gmail.com>
2024-02-01 22:42:48 +0000

    Add styles

--- main.js
+++ main.js
@@ -1,1 +1,10 @@
-// Add JS here
+const textoIngresado = document.getElementById('texto-ingresado');
+const textoResultado = document.getElementById('texto-resultado');
+const btnEncriptar = document.getElementById('btn-encriptar');
+const btnDesencriptar = document.getElementById('btn-desencriptar');
+const textoVacio = document.getElementById('sin-palabras');
+const btnCopy = document.getElementById('btn-copy');
+
+function encriptar(){
+
+}
